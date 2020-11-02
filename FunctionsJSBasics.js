71f3195928f78c787c7087b7b94760a8d4339a9c
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-11-02 14:02:14 +0530

    JS functions basics

---
+++ FunctionsJSBasics.js
@@ -0,0 +1,10 @@
+let id=1
+let name='Keerthana'
+let gender='female'
+const hasHobbies=true
+
+function userInfo(userName,userGender,userHasHobbies){
+    return ('Name is '+name+', the gender is ' +gender+' and hobbies is '+hasHobbies)
+}
+
+console.log(userInfo(name,gender,hasHobbies))
